ec01e3a8ea81c313054c5883c298fea3078c19ae
Randy <kriosen03@outlook.com>
2022-01-19 17:48:21 +0100

    Stripping arg in the find functions

--- utils.py
+++ utils.py
@@ -52,6 +52,7 @@ async def reaction_checker(self, message: discord.Message, embeds: list) -> None
             break
 
 def find_user(self, arg, secondary_database: bool = False):
+    arg = arg.strip()
     found = False
 
     if secondary_database:
@@ -124,6 +125,7 @@ def find_user(self, arg, secondary_database: bool = False):
     return {}   
 
 async def find_nation(arg: Union[str, int]) -> Union[dict, None]:
+    arg = arg.strip()
     try:
         result = list(mongo.world_nations.find({"nation": arg}).collation(
             {"locale": "en", "strength": 1}))[0]
@@ -141,6 +143,7 @@ async def find_nation(arg: Union[str, int]) -> Union[dict, None]:
     return result
 
 async def find_nation_plus(self, arg: Union[str, int]) -> Union[dict, None]: # only returns a nation if it is at least 1 day old
+    arg = arg.strip()
     nation = await find_nation(arg)
     if nation == None:
         nation = find_user(self, arg)
