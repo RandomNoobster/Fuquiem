11132ee2372e8171d4863d5bf60e690931c024da
Randy <kriosen03@outlook.com>
2022-03-12 20:31:01 +0100

    Adding message_content intent

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import discord
 from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
 load_dotenv()
 
 client = pymongo.MongoClient(os.getenv("pymongolink"))
