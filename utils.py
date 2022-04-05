beba0854e3047debb6878cb5238dd0b5c3397fbc
Randy <kriosen03@outlook.com>
2022-04-05 20:24:21 +0200

    Adding a call function

--- utils.py
+++ utils.py
@@ -72,6 +72,21 @@ high_gov_plus_perms = [
     pontifex_id
 ]
 
+async def call(data: dict = None, key: str = api_key) -> Union[dict, aiohttp.ClientResponse]:
+    async with aiohttp.ClientSession() as session:
+        data = "{nations(page:1 first:500 alliance_position:[2,3,4,5] alliance_id:4729){data{id nation_name num_cities}}}"
+        while True:
+            async with session.post(f'https://api.politicsandwar.com/graphql?api_key={key}', json={"query": data}) as response:
+                if response.headers['X-RateLimit-Remaining'] == 0:
+                    await asyncio.sleep(response.headers['X-RateLimit-Reset-After'])
+                    continue
+                json_response = await response.json()
+                try:
+                    errors = json_response['errors']
+                except:
+                    errors = None
+                return json_response
+
 def embed_pager(title: str, fields: list, description: str = "", color: int = 0x00ff00, inline: bool = True) -> list:
     embeds = []
     for i in range(math.ceil(len(fields)/24)):
